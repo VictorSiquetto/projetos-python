bd02f0945dcac6d2c0168cd46706e43d1f84e74f
Victor Siquetto <victor.siquetto@gmail.com>
2026-09-18 21:43:41 -0300

    exercicio finalizado

---
+++ exercicios-python/Mundo 4/ex009/__main__.py
@@ -0,0 +1,15 @@
+from classes import *
+
+def main():
+    bebida1 = Cafe()
+    bebida1.preparar()
+
+    bebida2 = Cha()
+    bebida2.preparar()
+
+    bebida3 = Leite()
+    bebida3.preparar()
+
+
+if __name__ == "__main__":
+    main()
